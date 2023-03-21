5340e9cc47efcb919c64f71ffb781e2296d2eba6
stkd97 <stkd97@gmail.com>
2023-03-21 16:46:21 +0000

    lab3

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -11,9 +11,29 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 using namespace std;
 
 void TCPReceiver::segment_received(const TCPSegment &seg) {
-    DUMMY_CODE(seg);
+    const TCPHeader& header = seg.header();
+    const string payload = seg.payload().copy();
+
+    if (!is_syn && header.syn) isn = WrappingInt32(header.seqno);
+
+    is_syn |= header.syn;
+    is_fin = (is_fin & !header.syn) | header.fin;
+
+    const uint64_t checkpoint = _reassembler.stream_out().bytes_written();
+    const WrappingInt32 seqno(header.seqno + (header.syn ? 0 : -1));
+    const uint64_t pos = unwrap(seqno, isn, checkpoint);
+
+    _reassembler.push_substring(payload, pos, header.fin);
 }
 
-optional<WrappingInt32> TCPReceiver::ackno() const { return {}; }
+optional<WrappingInt32> TCPReceiver::ackno() const {
+    if (!is_syn) return std::nullopt;
 
-size_t TCPReceiver::window_size() const { return {}; }
+    const uint64_t checkpoint = _reassembler.stream_out().bytes_written();
+    uint64_t size = checkpoint + is_syn + (is_fin && _reassembler.empty());
+    return wrap(size, isn);
+}
+
+size_t TCPReceiver::window_size() const { 
+    return _capacity - _reassembler.stream_out().buffer_size();
+}
--- libsponge/tcp_receiver.hh
+++ libsponge/tcp_receiver.hh
@@ -20,6 +20,10 @@ class TCPReceiver {
     //! The maximum number of bytes we'll store.
     size_t _capacity;
 
+    WrappingInt32 isn{0};
+    bool is_syn = false;
+    bool is_fin = false;
+
   public:
     //! \brief Construct a TCP receiver
     //!
